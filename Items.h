a3afb49e7136725e4a51e63bec58d1713a797a3f
gdosulli <45126172+gdosulli@users.noreply.github.com>
2020-02-23 18:52:04 -0500

    Added remaining function declarations to class. Added overloaded << operator definition

--- Items.h
+++ Items.h
@@ -19,7 +19,35 @@ public:
     // Requires: ostream and items object
     // Modifies: nothing
     // Effects: prints out all objects held in list
-    friend ostream& operator << (ostream& outs, const Items &items);
+    friend ostream& operator << (ostream& outs, const Items &items) {
+        outs << "Items held in list vector: ";
+        for (int i = 0; i < items.list.size(); i++) {
+            if (i == 0) {
+                outs << items.list.at(i);
+            } else {
+                outs << ", " << items.list.at(i);
+            }
+        }
+        return outs;
+    }
+
+    // Requires: nothing
+    // Modifies: list
+    // Effects: sorts list of objects in ascending order of value
+    void sort();
+
+    // Requires: Object to search for in list
+    // Modifies: nothing
+    // Effects: Searches for specified item in list. return true if found and false otherwise
+    bool find(Object object);
+
+    // Randomization Methods
+    // Requires: nothing
+    // Modifies: list
+    // Effects: randomizes order of objects in list
+    void randomizeSC();
+    void randomizeME();
+    void randomizeGO();
 
 private:
     vector<Object> list;
