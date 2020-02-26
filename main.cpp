688453dad623f3fe7dc25c4f5edb3c2cf40f289d
gdosulli <45126172+gdosulli@users.noreply.github.com>
2020-02-26 12:29:05 -0500

    Added test for vector sorting

--- main.cpp
+++ main.cpp
@@ -6,11 +6,22 @@
 #include "Items.h"
 #include "Tile.h"
 #include <iostream>
+#include <random>
 
 using namespace std;
 
 int main() {
     Tile test = Tile(6, "Wood");
     cout << test << endl;
+
+    // Test sort
+    vector<int> testVect;
+    for (int i = 0; i < 30; i++) {
+        testVect.push_back(rand() % 25);
+    }
+    Items<int> items(testVect);
+    cout << "Unsorted elements: " << items << endl;
+    items.sort();
+    cout << "Sorted elements: " << items << endl;
     return 0;
 }
